19eebaee5549dd1792c3ada6d31ba9fc377b41a7
Mubens <58505335+Mubens@users.noreply.github.com>
2020-07-24 21:37:01 +0800

    node
    node study

---
+++ Node.js/commonjs-test/a.js
@@ -0,0 +1,10 @@
+const calculator = {
+  add(n1, n2) {
+    return n1 + n2
+  },
+  mul(n1, n2) {
+    return n1 * n2
+  }
+}
+
+module.exports = calculator
---
+++ Node.js/commonjs-test/b.js
@@ -0,0 +1,6 @@
+const calculator = require('./a')
+
+let num = calculator.add(1, 10)
+num = calculator.mul(num, 100)
+
+console.log(num)
