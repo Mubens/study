d93da53411e0d02604bcf6bb7cfa4202501c00e1
Mubens <58505335+Mubens@users.noreply.github.com>
2020-08-08 23:35:53 +0800

    update

--- 各种控件/drag 拖拽排序/js/index.js
+++ 各种控件/drag 拖拽排序/js/index.js
@@ -1,47 +1,40 @@
-;
-(function dragAction() {
-    // 使用事件委托
-    // 委托元素 node
-    var node = document.querySelector('#drag-items');
-    // 鼠标拖动项
-    var dragging = null;
-    // 覆盖的目标
-    var dragTarget = null;
-    var start;
-    var f = false;
-    var last = { x: null, y: null };
-    // 拖动开始事件
-    node.addEventListener('dragstart', function (e) {
-        // 设置一张透明的图片去除默认的预览图
-        var img = new Image();
-        img.src =
-            "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' %3E%3Cpath /%3E%3C/svg%3E";
-        e.dataTransfer.setDragImage(img, 0, 0);
-        dragging = e.target;
-        start = { x: e.clientX, y: e.clientY };
-        var target = e.target;
-        target.classList.add('dragging');
-    });
-    // 拖动到放置目标上时
-    node.addEventListener('dragover', function (e) {
-        e.preventDefault();
-        var target = e.target;
-        // 节流：必须是可拖动的元素 && 放置目标必须与之前那次不同
-        if (isDragItem(target) && target !== dragTarget) {
-            dragTarget = target;
-        }
-    });
-    // 拖动过程中
-    node.addEventListener('drag', function (e) {
-        var target = e.target;
-        var move = {
-            x: e.clientX - start.x,
-            y: e.clientY - start.y
-        };
-        var minX = -(getIndex(target) % 5) * 112;
-        var minY = (getIndex(target) < 5 ? 0 : -1) * 112;
-        var maxX = node.clientWidth - ((getIndex(target) % 5) + 1) * 112;
-        var maxY = node.clientHeight - Math.floor(getIndex(target) / 5 + 1) * 112;
+// 使用事件委托
+// 委托元素 node
+var node = document.querySelector('#drag-items');
+// 鼠标拖动项
+var dragging = null;
+// 覆盖的目标
+var dragTarget = null;
+var start;
+// 拖动开始事件
+node.addEventListener('dragstart', function (e) {
+    // 设置一张透明的图片去除默认的预览图
+    var img = new Image();
+    img.src = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' %3E%3Cpath /%3E%3C/svg%3E";
+    e.dataTransfer.setDragImage(img, 0, 0);
+    dragging = e.target;
+    start = { x: e.clientX, y: e.clientY };
+    var target = e.target;
+    target.classList.add('dragging');
+});
+// 拖动到放置目标上时
+node.addEventListener('dragover', function (e) {
+    e.preventDefault();
+    var target = e.target;
+    // 必须是可拖动的元素 && 放置目标必须与之前那次不同
+    if (isDragItem(target) && target !== dragTarget) {
+        dragTarget = target;
+    }
+});
+// 拖动过程中
+node.addEventListener('drag', function (e) {
+    var target = e.target;
+    if (isDragItem(target)) {
+        var move = { x: e.clientX - start.x, y: e.clientY - start.y };
+        var minX = node.offsetLeft - target.offsetLeft;
+        var minY = node.offsetTop - target.offsetTop;
+        var maxX = node.offsetLeft + node.clientWidth - (target.offsetLeft + target.clientWidth);
+        var maxY = node.offsetTop + node.clientHeight - (target.offsetTop + target.clientHeight);
         if (move.x < minX) {
             move.x = minX;
         }
@@ -54,84 +47,44 @@
         if (move.y > maxY) {
             move.y = maxY;
         }
-        // 限制范围后，最后一次触发会产生 bug
-        if (move.x > 0 || move.y > 0) {
-            target.style.transform = "translate(" + move.x + "px, " + move.y + "px)";
+        // 限制范围后，最后一次触发事件时动画会产生 bug ，需要过滤这一次的 transform
+        if ((move.x === 0 && move.y === 0) || (move.x === minX && move.y === 0) || (move.x === minX && move.y === minY))
             return;
-        }
-        if (move.x < 0 &&
-            move.x !== -(getIndex(target) % 5) * 112 &&
-            move.y === 0) {
-            target.style.transform = "translate(" + move.x + "px, " + move.y + "px)";
-            return;
-        }
-        if (move.x <= 0 && move.y < 0 && move.y !== -112) {
-            target.style.transform = "translate(" + move.x + "px, " + move.y + "px)";
-            return;
-        }
-    });
-    // 拖拽动作结束事件
-    node.addEventListener('dragend', function (e) {
-        var target = e.target;
-        target.classList.add('back-anime');
-        setTimeout(function () {
-            target.classList.remove('back-anime');
-            target.style.transform = 'translate(0px, 0px)';
-            target.classList.remove('dragging');
-        }, 300);
-        dragging = null;
-        dragTarget = null;
-    });
-    // 获取当前拖动的元素在父容器中的 index
-    function getIndex(el) {
-        if (!el || !el.parentNode) {
-            return -1;
-        }
-        var index = 0;
-        var children = el.parentNode.children;
-        for (var i = 0, length_1 = children.length; i < length_1; i++) {
-            var child = children[i];
-            // index 自增条件：必须是可拖动的元素 && 元素必须不是当前拖动的元素
-            if (isDragItem(child) && child !== el) {
-                index++;
-            }
-            else {
-                return index;
-            }
-        }
+        target.style.transform = "translate(" + move.x + "px, " + move.y + "px)";
+    }
+});
+// 拖拽动作结束事件
+node.addEventListener('dragend', function (e) {
+    var target = e.target;
+    target.classList.add('back-anime');
+    setTimeout(function () {
+        target.classList.remove('back-anime');
+        target.style.transform = 'translate(0px, 0px)';
+        target.classList.remove('dragging');
+    }, 300);
+    dragging = null;
+    dragTarget = null;
+});
+// 获取当前拖动的元素在父容器中的 index
+function getIndex(el) {
+    if (!el || !el.parentNode) {
         return -1;
     }
-    // 判断元素是否是可拖拽的元素
-    function isDragItem(target) {
-        return target.classList.value.indexOf('drag-item') > -1 ? true : false;
+    var index = 0;
+    var children = el.parentNode.children;
+    for (var i = 0, length_1 = children.length; i < length_1; i++) {
+        var child = children[i];
+        // index 自增条件：必须是可拖动的元素 && 元素必须不是当前拖动的元素
+        if (isDragItem(child) && child !== el) {
+            index++;
+        }
+        else {
+            return index;
+        }
     }
-    var a;
-    // 限制坐标移动范围
-    function rangePosition(el, pos) {
-        var index = getIndex(el);
-        var x = pos.x;
-        var y = pos.y;
-        var minX = -el.clientWidth * index;
-        var minY = -el.clientHeight * (index < 5 ? 0 : 1);
-        var maxX = node.clientWidth - el.clientWidth * ((index % 5) + 1);
-        var maxY = node.clientHeight - el.clientHeight * (index < 5 ? 1 : 2);
-        if (x < minX)
-            x = minX;
-        if (x > maxX)
-            x = maxX;
-        if (y < minY)
-            y = minY;
-        if (y > maxY)
-            y = maxY;
-        console.log({ x: x, y: y, minX: minX, minY: minY, maxX: maxX, maxY: maxY });
-        return { x: x, y: y };
-    }
-    function getTransformPositon(str) {
-        var reg = /(\d+).*?(\d+)/s;
-        var res = reg.exec(str || '0,0');
-        return {
-            x: res[1],
-            y: res[2]
-        };
-    }
-})();
+    return -1;
+}
+// 判断元素是否是可拖拽的元素
+function isDragItem(target) {
+    return target.classList.value.indexOf('drag-item') > -1 ? true : false;
+}
--- 各种控件/drag 拖拽排序/js/index.ts
+++ 各种控件/drag 拖拽排序/js/index.ts
@@ -1,4 +1,4 @@
-;(function dragAction() {
+;(function dragItemAndSwitch() {
   // 使用事件委托
   // 委托元素 node
   const node = document.querySelector('#drag-items') as HTMLElement
@@ -9,20 +9,17 @@
   }
 
   // 鼠标拖动项
-  let dragging = null
+  let dragging: any = null
   // 覆盖的目标
-  let dragTarget = null
+  let dragTarget: any = null
   let start: IPosition
-  let f = false
-  let last: IPosition = { x: null, y: null }
 
   // 拖动开始事件
   node.addEventListener('dragstart', function (e: DragEvent) {
     // 设置一张透明的图片去除默认的预览图
     const img = new Image()
-    img.src =
-      "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' %3E%3Cpath /%3E%3C/svg%3E"
-    e.dataTransfer.setDragImage(img, 0, 0)
+    img.src = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' %3E%3Cpath /%3E%3C/svg%3E"
+    e.dataTransfer && e.dataTransfer.setDragImage(img, 0, 0)
 
     dragging = e.target
     start = { x: e.clientX, y: e.clientY }
@@ -36,7 +33,7 @@
     e.preventDefault()
     const target = e.target as HTMLElement
 
-    // 节流：必须是可拖动的元素 && 放置目标必须与之前那次不同
+    // 必须是可拖动的元素 && 放置目标必须与之前那次不同
     if (isDragItem(target) && target !== dragTarget) {
       dragTarget = target
     }
@@ -44,53 +41,37 @@
 
   // 拖动过程中
   node.addEventListener('drag', function (e: DragEvent) {
-    let target = e.target as HTMLElement
-    let move = {
-      x: e.clientX - start.x,
-      y: e.clientY - start.y
-    }
-    let minX = -(getIndex(target) % 5) * 112
-    let minY = (getIndex(target) < 5 ? 0 : -1) * 112
-    let maxX = node.clientWidth - ((getIndex(target) % 5) + 1) * 112
-    let maxY = node.clientHeight - Math.floor(getIndex(target) / 5 + 1) * 112
+    const target = e.target as HTMLElement
+    if (isDragItem(target)) {
+      let move = { x: e.clientX - start.x, y: e.clientY - start.y }
+      let minX = node.offsetLeft - target.offsetLeft
+      let minY = node.offsetTop - target.offsetTop
+      let maxX = node.offsetLeft + node.clientWidth - (target.offsetLeft + target.clientWidth)
+      let maxY = node.offsetTop + node.clientHeight - (target.offsetTop + target.clientHeight)
 
-    if (move.x < minX) {
-      move.x = minX
-    }
-    if (move.x > maxX) {
-      move.x = maxX
-    }
-    if (move.y < minY) {
-      move.y = minY
-    }
-    if (move.y > maxY) {
-      move.y = maxY
-    }
+      if (move.x < minX) {
+        move.x = minX
+      }
+      if (move.x > maxX) {
+        move.x = maxX
+      }
+      if (move.y < minY) {
+        move.y = minY
+      }
+      if (move.y > maxY) {
+        move.y = maxY
+      }
 
-    // 限制范围后，最后一次触发会产生 bug
-    if (move.x > 0 || move.y > 0) {
+      // 限制范围后，最后一次触发事件时动画会产生 bug ，需要过滤这一次的 transform
+      if ((move.x === 0 && move.y === 0) || (move.x === minX && move.y === 0) || (move.x === minX && move.y === minY))
+        return
       target.style.transform = `translate(${move.x}px, ${move.y}px)`
-      return
-    }
-
-    if (
-      move.x < 0 &&
-      move.x !== -(getIndex(target) % 5) * 112 &&
-      move.y === 0
-    ) {
-      target.style.transform = `translate(${move.x}px, ${move.y}px)`
-      return
-    }
-
-    if (move.x <= 0 && move.y < 0 && move.y !== -112) {
-      target.style.transform = `translate(${move.x}px, ${move.y}px)`
-      return
     }
   })
 
   // 拖拽动作结束事件
   node.addEventListener('dragend', function (e: DragEvent) {
-    let target = e.target as HTMLElement
+    const target = e.target as HTMLElement
     target.classList.add('back-anime')
     setTimeout(() => {
       target.classList.remove('back-anime')
@@ -124,33 +105,5 @@
   // 判断元素是否是可拖拽的元素
   function isDragItem(target: HTMLElement) {
     return target.classList.value.indexOf('drag-item') > -1 ? true : false
-  }
-
-  let a: IPosition
-  // 限制坐标移动范围
-  function rangePosition(el: HTMLElement, pos: IPosition): IPosition {
-    const index = getIndex(el)
-    let x = pos.x
-    let y = pos.y
-    const minX = -el.clientWidth * index
-    const minY = -el.clientHeight * (index < 5 ? 0 : 1)
-    const maxX = node.clientWidth - el.clientWidth * ((index % 5) + 1)
-    const maxY = node.clientHeight - el.clientHeight * (index < 5 ? 1 : 2)
-
-    if (x < minX) x = minX
-    if (x > maxX) x = maxX
-    if (y < minY) y = minY
-    if (y > maxY) y = maxY
-    console.log({ x, y, minX, minY, maxX, maxY })
-    return { x, y }
-  }
-
-  function getTransformPositon(str: string) {
-    const reg = /(\d+).*?(\d+)/s
-    const res = reg.exec(str || '0,0')
-    return {
-      x: res[1],
-      y: res[2]
-    }
   }
 })()
